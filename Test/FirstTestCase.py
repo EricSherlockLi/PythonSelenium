c1f9ac4830eca1d8825520bb4ce81e024ede35d8
EricSherlockLi <helloleolee@hotmail.com>
2023-08-30 17:41:32 +0800

    URL commit

--- Test/FirstTestCase.py
+++ Test/FirstTestCase.py
@@ -7,3 +7,11 @@
 # 6. Capture title of the home page
 # 7. Verify title of the page: OrangeHRM
 # 8. Close browser
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(executable_path=r"C:\Users\hello\PythonSelenium\Test\webdriver\chromedriver.exe", options=chrome_options)
